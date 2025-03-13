0626036a75c4044544c7c2365bd71e7bc05a8e3b
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-03-12 23:43:30 -0300

    subiendo todo

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -3,6 +3,14 @@ import { NextResponse, } from 'next/server'
 export async function GET(_request: Request) {
 
     return NextResponse.json({
-        message: 'Next.js desde el servidor'
+        message: 'Next.js get el servidor'
+    })
+}
+
+
+export async function POST(_request: Request) {
+
+    return NextResponse.json({
+        message: 'Next.js post el servidor'
     })
 }
