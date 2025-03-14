9d007366e8e8d938434b97d278c14d16c16c835d
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-03-14 18:31:29 -0300

    paginacion take skip

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -3,9 +3,24 @@ import { NextResponse, NextRequest } from 'next/server'
 
 export async function GET(request: Request) {
 
+    const { searchParams } = new URL(request.url);
+    const take = +(searchParams.get('take') ?? "10")
+    const skip = +(searchParams.get('skip') ?? "0")
 
+    if (isNaN(take)) {
+        return NextResponse.json(
+            { message: 'Take debe ser un número' },
+            { status: 400 }
+        )
+    }
+    if (isNaN(skip)) {
+        return NextResponse.json(
+            { message: 'skip debe ser un número' },
+            { status: 400 }
+        )
+    }
 
-    const todos = await prisma.todo.findMany()
+    const todos = await prisma.todo.findMany({ take, skip })
 
     return NextResponse.json({ message: 'TODO', todos })
 
