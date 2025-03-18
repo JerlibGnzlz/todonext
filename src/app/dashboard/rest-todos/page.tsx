4a68d08dc3ac507dc5b8ee47924fb14f14913109
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-03-18 00:57:01 -0300

    design de todos

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,8 +1,18 @@
+import prisma from "@/lib/schema";
 
+export const metadata = {
+    title: 'List of todos',
+}
 export default function RestTodosPage() {
+
+    const todos = prisma.todo.findMany({
+        orderBy: {
+            createdAt: "asc"
+        }
+    })
     return (
         <div>
-            <h1>RestTodos</h1>
+            {JSON.stringify(todos)}
         </div>
     );
 }
