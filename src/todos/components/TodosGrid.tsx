40fcc400351fe35cdc19bb54649db2f0a87859ed
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-03-18 03:43:02 -0300

    fix todo item

--- src/todos/components/TodosGrid.tsx
+++ src/todos/components/TodosGrid.tsx
@@ -3,6 +3,7 @@
 import { Todo } from "@prisma/client"
 import { TodoItem } from './TodoItem';
 
+import * as api from '../helpers/todos'
 
 interface TodosGridProps {
     todos?: Todo[]
@@ -12,7 +13,7 @@ export const TodosGrid = ({ todos = [] }: TodosGridProps) => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             {todos.map((todo) => (
-                <TodoItem key={todo.id} todo={todo} />
+                <TodoItem key={todo.id} todo={todo} toggleTodo={api.updateTodo} />
             ))}
         </div>
     );
