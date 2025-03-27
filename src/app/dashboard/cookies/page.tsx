2f099b4a74f7fc4b6b5bbf8e6cb731dfb6a87199
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-03-27 00:48:57 -0300

    cookies

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -1,4 +1,5 @@
 import { TabBar } from "@/components/TabBar";
+import { cookies } from "next/headers";
 
 
 export const metadata = {
@@ -7,15 +8,20 @@ export const metadata = {
 };
 
 
-export default function CokiesPage() {
+export default async function CokiesPage() {
+
+    const cookieStore = await cookies();
+    const cookieTab = cookieStore.get('selectTab')?.value ?? "1"
+
+
     return (
         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
             <div className="flex col-span-2">
                 <span className="text-3xl">TabBar</span>
             </div>
             <TabBar
-                tabOptions={[1, 2, 3, 4]}
-                currentTab={1}
+                // tabOptions={[1, 2, 3, 4]}
+                currentTab={+cookieTab}
             />
         </div>
     );
