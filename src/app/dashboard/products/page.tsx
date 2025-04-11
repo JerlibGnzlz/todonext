9f6cd0bc4e1ff38c767f6cc455d45d5e1dffa2b0
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-04-11 16:37:57 -0300

    fix image productos

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,9 +1,22 @@
 import { ProductCard } from "@/products/components/ProductCard";
+import { products } from "@/products/data/products";
 
 export default function ProductsPage() {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-            <ProductCard />
+
+            {
+                products.map(product => (
+                    <ProductCard key={product.id} {...product} />
+                ))
+            }
+            <ProductCard
+                id="UUID-ABC-1"
+                name="Teslo Hoodie"
+                price={15}
+                rating={5}
+                image="/images/products/buzo.jpg"
+            />
         </div>
     );
 }
